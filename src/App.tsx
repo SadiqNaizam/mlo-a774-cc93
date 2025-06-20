eb8e20b79182c0ab8dab5c09c248e7294336d519
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-20 13:07:51 +0530

    Updated code files: App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
 import ForgotPasswordPage from "./pages/ForgotPasswordPage";
 import LoginPage from "./pages/LoginPage";
 import PostLoginLandingPage from "./pages/PostLoginLandingPage";
@@ -12,11 +11,14 @@ import RegistrationPage from "./pages/RegistrationPage";
 import ResetPasswordPage from "./pages/ResetPasswordPage";
 import NotFound from "./pages/NotFound";
 
+import { ThemeProvider } from "./components/ThemeProvider"; // Added import
+
 const queryClient = new QueryClient();
 
 
 const App = () => (
 <QueryClientProvider client={queryClient}>
+  <ThemeProvider defaultTheme="light" storageKey="app-ui-theme"> {/* Wrapped with ThemeProvider */}
     <TooltipProvider>
     <Toaster />
     <Sonner />
@@ -36,6 +38,7 @@ const App = () => (
         </Routes>
     </BrowserRouter>
     </TooltipProvider>
+  </ThemeProvider>
 </QueryClientProvider>
 );
 
